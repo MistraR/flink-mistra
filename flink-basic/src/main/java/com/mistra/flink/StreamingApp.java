b023fd6d3fbb77d5414585ed7dd6b94b6f039786
rui.wang@yamu.com <842404548@qq.com>
2023-05-11 23:09:19 +0800

    word count 样例

--- flink-basic/src/main/java/com/mistra/flink/StreamingApp.java
+++ flink-basic/src/main/java/com/mistra/flink/StreamingApp.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -13,14 +14,16 @@ import org.apache.flink.util.Collector;
  * @author wrmistra@gmail.com
  * @date 2023/5/11
  * @ Description: word count 样例
+ * 终端开启websocket ： nc -lk 9888  往9888端口发送数据
+ * pk,pk,pk,flink,flink
  */
 public class StreamingApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
         // 对接数据源
-        DataStreamSource<String> source = env.socketTextStream("localshot", 9888);
+        DataStreamSource<String> source = env.socketTextStream("localhost", 9888);
 
         source.flatMap(new FlatMapFunction<String, String>() {
             @Override
@@ -41,6 +44,13 @@ public class StreamingApp {
             public Tuple2<String, Integer> map(String s) throws Exception {
                 return new Tuple2<>(s, 1);
             }
-        }).keyBy(0).sum(1).print();
+        }).keyBy(new KeySelector<Tuple2<String, Integer>, Object>() {
+            @Override
+            public Object getKey(Tuple2<String, Integer> stringIntegerTuple2) throws Exception {
+                return stringIntegerTuple2.f0;
+            }
+        }).sum(1).print();
+
+        env.execute("Fraud Detection");
     }
 }
